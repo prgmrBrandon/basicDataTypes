24e727cd27cd8291cb83d74c5cae3ee01a517125
Brandon Haulk <brandonhaulk@gmail.com>
2019-08-28 15:02:41 -0400

    Basic data types practice.

---
+++ basicDataTypes_BrandonHaulk.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    // Int ("%d"): 32 Bit integer
+    // Long ("%ld"): 64 bit integer
+    // Char ("%c"): Character type
+    // Float ("%f"): 32 bit real value
+    // Double ("%lf"): 64 bit real value
+
+    //Variables with sample data.
+    int num1 = 3;
+    long num2 = 12345678912345;
+    char letter1 = 'a';
+    float num3 = 334.23;
+    double num4 = 14049.30493;
+
+    printf("%d \n%ld \n%c \n%.3f \n%.9lf", num1, num2, letter1, num3, num4);
+}
