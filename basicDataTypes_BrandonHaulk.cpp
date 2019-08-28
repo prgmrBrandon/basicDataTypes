26f0238857a0bb82564736e258b9e3f22a2fb255
Brandon Haulk <brandonhaulk@gmail.com>
2019-08-28 15:16:11 -0400

    Refactor code.

--- basicDataTypes_BrandonHaulk.cpp
+++ basicDataTypes_BrandonHaulk.cpp
@@ -9,12 +9,17 @@ int main() {
     // Float ("%f"): 32 bit real value
     // Double ("%lf"): 64 bit real value
 
-    //Variables with sample data.
-    int num1 = 3;
-    long num2 = 12345678912345;
-    char letter1 = 'a';
-    float num3 = 334.23;
-    double num4 = 14049.30493;
+    //Variables
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%ld\n%c\n%.03f\n%.09lf", a, b, c, d, e);
+    
+    return 0;
+}
 
-    printf("%d \n%ld \n%c \n%.3f \n%.9lf", num1, num2, letter1, num3, num4);
 }
